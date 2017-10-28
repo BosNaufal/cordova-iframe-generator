6a9754e9448d41503686219a5bb0360da0504efb
Naufal <bosnaufalemail@gmail.com>
2017-10-29 06:51:37 +0700

    Fix UI

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ const upload = multer({ dest: "storages/" })
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use("/static", express.static("public"));
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "views", "index.html"))
@@ -18,6 +19,6 @@ app.post("/create", upload.any(), require("./controllers/CordovaCreate.js"))
 app.post("/compile/:appName", require("./controllers/CordovaCompile.js"))
 app.get("/download/:appName", require("./controllers/Download.js"))
 
-app.listen(3000, function () {
-  console.log("Listening to port 3000")
+app.listen(9000, function () {
+  console.log("Listening to port 9000")
 })
